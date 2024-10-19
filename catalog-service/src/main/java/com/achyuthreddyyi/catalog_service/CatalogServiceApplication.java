49d0b8a86d8f481d5eafd6f98a14ef708989ee78
Achyuth Reddy <ncsachyuth@gmail.com>
2024-10-19 12:57:59 +0530

    Test: Dummy commit
    - To check if github workflows gets triggered

--- catalog-service/src/main/java/com/achyuthreddyyi/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/achyuthreddyyi/catalog_service/CatalogServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("Starting Catalog Service Application...");
+
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 }
