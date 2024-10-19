4905a7e48aaaef6a7fb5883e0ac4f1d559150e24
Achyuth Reddy <ncsachyuth@gmail.com>
2024-10-19 19:45:05 +0530

    Feat: Implements get Products API
    - Implemented pagintion along with the get products API.
    - Implemented database entities & records for the data transfer

--- catalog-service/src/main/java/com/achyuthreddyyi/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/achyuthreddyyi/catalog_service/CatalogServiceApplication.java
@@ -2,8 +2,12 @@ package com.achyuthreddyyi.catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
