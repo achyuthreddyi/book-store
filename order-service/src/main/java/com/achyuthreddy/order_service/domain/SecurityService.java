2526e1f485c347791840621ff8271714732e3e10
Achyuth Reddy <ncsachyuth@gmail.com>
2024-10-21 11:29:31 +0530

    feat: Implements dummy security service to authenticate users

---
+++ order-service/src/main/java/com/achyuthreddy/order_service/domain/SecurityService.java
@@ -0,0 +1,10 @@
+package com.achyuthreddy.order_service.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+    public String getLoginUser(){
+        return "user";
+    }
+}
