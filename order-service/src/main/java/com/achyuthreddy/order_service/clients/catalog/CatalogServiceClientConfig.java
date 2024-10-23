d43640752bc7aecd238f0d3ce109d43b9e1e6732
Achyuth Reddy <ncsachyuth@gmail.com>
2024-10-23 15:28:49 +0530

    feat: Implements Timeout in restClient configuration.
    - Adds 5 sec duration to establish and receiving the response.

--- order-service/src/main/java/com/achyuthreddy/order_service/clients/catalog/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/achyuthreddy/order_service/clients/catalog/CatalogServiceClientConfig.java
@@ -1,14 +1,24 @@
 package com.achyuthreddy.order_service.clients.catalog;
 
 import com.achyuthreddy.order_service.ApplicationProperties;
+
+import java.time.Duration;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+
+
 
 @Configuration
 public class CatalogServiceClientConfig {
     @Bean
     RestClient restClient(ApplicationProperties properties) {
-        return RestClient.builder().baseUrl(properties.catalogServiceUrl()).build();
+        return RestClient.builder().baseUrl(properties.catalogServiceUrl())
+                .requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS
+                        .withConnectTimeout(Duration.ofSeconds(5)) // Max time that can be taken to establish connection
+                        .withReadTimeout(Duration.ofSeconds(5)))) // Time out for receiving response from service.
+                .build();
     }
 }
