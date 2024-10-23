80ba90ddebbcb51c5b91e28042acb4f96a46d50f
Achyuth Reddy <ncsachyuth@gmail.com>
2024-10-23 14:41:44 +0530

    fix: Fixes file name for the catalog service config.

--- order-service/src/main/java/com/achyuthreddy/order_service/clients/catalog/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/achyuthreddy/order_service/clients/catalog/CatalogServiceClientConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-public class CatalogServiceClientCatalog {
+public class CatalogServiceClientConfig {
     @Bean
     RestClient restClient(ApplicationProperties properties) {
         return RestClient.builder().baseUrl(properties.catalogServiceUrl()).build();
