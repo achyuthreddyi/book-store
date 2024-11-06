90d6c806b9d473ee1c0e8d57e57872a2bdecc66b
Achyuth Reddy <ncsachyuth@gmail.com>
2024-11-06 09:15:14 +0530

    test: mark get orders API test as pending for further investigation

--- order-service/src/test/java/com/achyuthreddy/order_service/web/controllers/OrderControllerTests.java
+++ order-service/src/test/java/com/achyuthreddy/order_service/web/controllers/OrderControllerTests.java
@@ -1,15 +1,22 @@
 package com.achyuthreddy.order_service.web.controllers;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import com.achyuthreddy.order_service.AbstractIT;
+import com.achyuthreddy.order_service.domain.models.OrderSummary;
 import com.achyuthreddy.order_service.testdata.TestDataFactory;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
+import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/test-orders.sql")
 class OrderControllerTests extends AbstractIT {
 
     @Nested
@@ -39,6 +46,37 @@ class OrderControllerTests extends AbstractIT {
                     .post("/api/orders")
                     .then()
                     .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
+    }
+
+    @Nested
+    class GetOrdersTests {
+        @Test
+        void shouldGetOrdersSuccessfully() {
+            List<OrderSummary> orderSummaries = given().when()
+                    .get("/api/orders")
+                    .then()
+                    .statusCode(200)
+                    .extract()
+                    .body()
+                    .as(new TypeRef<>() {});
+            System.out.println(orderSummaries);
+            assertThat(orderSummaries).hasSize(0);
+        }
+    }
+
+    @Nested
+    class GetOrderByOrderNumberTests {
+        String orderNumber = "order-123";
+
+        @Test
+        void shouldGetOrderSuccessfully() {
+            given().when()
+                    .get("/api/orders/{orderNumber}", orderNumber)
+                    .then()
+                    .statusCode(200)
+                    .body("orderNumber", is(orderNumber))
+                    .body("items.size()", is(2));
         }
     }
 }
