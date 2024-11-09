4981d4abe00edeffd4910eb61f92e800535b1f9b
Achyuth Reddy <ncsachyuth@gmail.com>
2024-11-09 12:14:56 +0530

    feat: Implements basic UI to display Products Page
    - Implements layouts & page page to display all the pages in the catalog.
    - Implements alpine JS methods to make an AJAX call to the API gateway.
    - Implements DedupeResponseHeader to avoid duplicate server URL's in the API call.

---
+++ bookstore-webapp/src/main/resources/static/js/products.js
@@ -0,0 +1,24 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('initData', (pageNo) => ({
+        pageNo: pageNo,
+        products: {
+            data: []
+        },
+        init() {
+            this.loadProducts(this.pageNo);
+            console.log("Init Data")
+
+        },
+        loadProducts(pageNo) {
+            $.getJSON("http://localhost:8989/catalog/api/products?page="+pageNo, (resp)=> {
+                console.log("Products Resp:", resp)
+                this.products = resp;
+            });
+        },
+        addToCart(product) {
+//            addProductToCart(product)
+              console.log("Adding product to cart", product)
+        }
+
+    }))
+});
